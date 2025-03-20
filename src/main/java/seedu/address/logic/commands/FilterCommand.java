a7c4910692ab5181dbba7daf6c3687588e0a856d
Matthan Foo <matthanfoo@gmail.com>
2025-03-20 10:37:44 +0800

    feat: add filter command functionality (filter contacts on tags)

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.person.PersonTagsContainsSearchPredicate;
+
+
+/**
+ * Finds and lists all persons in address book whose tags contains the exact search term.
+ * Search term matching is case-insensitive.
+ */
+public class FilterCommand extends Command {
+
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons with a tag that contains any of "
+            + "the exact (single) search term (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " alice bob charlie";
+
+    private final PersonTagsContainsSearchPredicate predicate;
+
+    public FilterCommand(String nameKeyWord) {
+        this.predicate = new PersonTagsContainsSearchPredicate(nameKeyWord);
+    }
+
+    public PersonTagsContainsSearchPredicate getPredicate() {
+        return predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FilterCommand)) {
+            return false;
+        }
+
+        FilterCommand otherFilterCommand = (FilterCommand) other;
+        return predicate.equals(otherFilterCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+}

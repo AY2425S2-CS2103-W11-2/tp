536a553fd3696beb803650129d86c13b93330713
Muhd Juraimi <muhd.juraimi123@gmail.com>
2025-03-22 00:45:35 +0800

    chore: remove unused imports

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -3,9 +3,6 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COMPANY;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
